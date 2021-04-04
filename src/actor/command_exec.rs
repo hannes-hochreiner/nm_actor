804d95f561331a1f7d77ad211aa96d4dfc7b5d94
Hannes Hochreiner <hannes@hochreiner.net>
2021-04-04 12:18:45 +0200

    add command status check

--- src/actor/command_exec.rs
+++ src/actor/command_exec.rs
@@ -1,5 +1,5 @@
 use std::process::Command;
-use super::exec::Exec;
+use super::{ActorError, exec::Exec};
 use std::error::Error;
 
 pub struct CommandExec {}
@@ -7,6 +7,10 @@ pub struct CommandExec {}
 impl Exec for CommandExec {
     fn exec(&mut self, command: &String, args: &Vec<&String>) -> Result<String, Box<dyn Error>> {
         let output = Command::new(command).args(args).output()?;
+
+        if !output.status.success() {
+            return Err(Box::new(ActorError::new(String::from("command failed"))));
+        }
 
         Ok(String::from_utf8(output.stdout)?)
     }
